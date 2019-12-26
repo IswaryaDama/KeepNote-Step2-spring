978a355252903a8173f1f573239628527cc51e32
Iswarya.Dama <iswarya.dama@cognizant.com>
2019-12-27 01:16:06 +0530

    Update ApplicationContextConfig.java

--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -30,10 +30,16 @@ public class ApplicationContextConfig {
 	@Bean
 	public DataSource datasource() {
 		BasicDataSource dataSource=new BasicDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	/*	dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/keep_note2");
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setPassword("root");*/
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
+				+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+		dataSource.setUsername(System.getenv("MYSQL_USER"));
+		dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); 
+
 	    return dataSource;
 	}
 	
